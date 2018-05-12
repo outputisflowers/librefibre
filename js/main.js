482f505eb22ac47632876b1df30eecbe41c7e23d
outputisflowers <37437942+outputisflowers@users.noreply.github.com>
2018-05-12 17:24:41 +0200

    touchmove4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-document.addEventListener('scroll', graphicAppear(e));
-document.addEventListener('touchmove', graphicAppear(e));
+document.addEventListener('scroll', graphicAppear());
+document.addEventListener('touchmove', graphicAppear());
 
 function graphicAppear(e){
     var divHeight = 0;
